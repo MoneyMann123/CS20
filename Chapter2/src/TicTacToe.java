1c7aef8d200c75494e9dcd937210e16ed3047843
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-12 12:31:00 -0600

    money

--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -1,9 +1,24 @@
-
-public class TicTacToe {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+/*
+Program: TicTacBoard.java          Date: September 7, 2022
+Author: Money Mann 
+School: CHHS
+Course: Computer Science 20
+ 
+*/
+public class TicTacToe
+{
+	public static void main(String[] args)
+	{
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		System.out.println("------------------------");
+		System.out.println("       |        |");
+		System.out.println("       |   x    |");
+		System.out.println("       |        |");
+		System.out.println("------------------------");
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		
 	}
-
 }
