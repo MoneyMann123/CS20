ed67954a33db1511d69e8f20a852f67cf99acbef
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-14 12:50:25 -0600

    money

--- Chapter3/src/SkillBuilding/RectanglePerimeter.java
+++ Chapter3/src/SkillBuilding/RectanglePerimeter.java
@@ -1,3 +1,9 @@
+/*
+	Program: RectanglePerimeter.java          Date: September 14, 2022
+	Author: Money Mann 
+	School: CHHS
+	Course: Computer Science 20
+*/
 package SkillBuilding;
 
 public class RectanglePerimeter 
@@ -5,8 +11,17 @@ public class RectanglePerimeter
 
 	public static void main(String[] args) 
 	{
-		
+		int varw = 4;
+		int varl = 13;
+		int ans = (2 * varw) + (2 * varl);
+		System.out.println("The perimeter of a rectangle with a width of 4 and length of 13:\n2w + 2l = " + ans);
 
 	}
 
 }
+/* Screen Dump
+The perimeter of a rectangle with a width of 4 and length of 13:
+2w + 2l = 34
+
+
+*/
