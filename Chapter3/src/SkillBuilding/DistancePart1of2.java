08e93a6c0e7662c34a5e2221cf503609795942ac
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-14 13:18:11 -0600

    money

--- Chapter3/src/SkillBuilding/DistancePart1of2.java
+++ Chapter3/src/SkillBuilding/DistancePart1of2.java
@@ -1,10 +1,31 @@
+/*
+	Program: DistancePart1of2.java          Date: September 14, 2022
+	Author: Money Mann 
+	School: CHHS
+	Course: Computer Science 20
+*/
 package SkillBuilding;
 
-public class DistancePart1of2 {
+import java.text.DecimalFormat;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class DistancePart1of2 
+{
+
+	public static void main(String[] args) 
+	{
+		DecimalFormat dc = new DecimalFormat("0.0");
+		
+		
+		double seg1 = 12.2;
+		double seg2 = 10.6;
+		double seg3 = 5.8;
+		double total = seg1 + seg2 + seg3;
+		System.out.println("The total distance of this race is " + dc.format(total) + "km.");
 
 	}
 
 }
+/* Screen Dump
+The total distance of this race is 28.6km.
+
+*/
