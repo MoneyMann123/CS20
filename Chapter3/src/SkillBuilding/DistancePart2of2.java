72ea6bcac66380925502dce1c8125d6561f13164
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-14 13:29:57 -0600

    money

---
+++ Chapter3/src/SkillBuilding/DistancePart2of2.java
@@ -0,0 +1,21 @@
+/*
+	Program: DistancePart2of2.java          Date: September 14, 2022
+	Author: Money Mann 
+	School: CHHS
+	Course: Computer Science 20
+*/
+package SkillBuilding;
+
+public class DistancePart2of2 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
+/* Screen Dump
+
+*/
