91b9d689159ead7aaffec1929e4738361c69dabd
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-19 12:32:49 -0600

    money

---
+++ Chapter3/src/SkillBuilding/GradeAvgPart2.java
@@ -0,0 +1,23 @@
+/*
+	Program: GradeAvgPart2.java          Date: September 19, 2022
+	Author: Money Mann 
+	School: CHHS
+	Course: Computer Science 20
+*/
+package SkillBuilding;
+
+import java.util.Scanner;
+
+public class GradeAvgPart2 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
+/* Screen Dump
+
+*/
